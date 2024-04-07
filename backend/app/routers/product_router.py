fe31a26abaadb727df7c18b1d1ab6607d00fdc39
Julian Lechner <jll38@njit.edu>
2024-04-07 17:48:35 -0400

    Product router work

--- backend/app/routers/product_router.py
+++ backend/app/routers/product_router.py
@@ -2,6 +2,15 @@ from fastapi import APIRouter, FastAPI
 
 product_router = APIRouter()
 
-@product_router.get('/products')
+@product_router.get('/product')
 def read_products():
-    return [{"name": "test"}]
+    pass
+
+@product_router.get('/product/${product_id}')
+def read_product():
+    pass
+
+@product_router.post('/product/${product_id}')
+def read_product(): 
+    pass
+
