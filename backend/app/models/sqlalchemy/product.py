f79eeb2758d64e28ed5b5be7d3bcbb427e9bc237
Julian Lechner <jll38@njit.edu>
2024-04-13 17:21:47 -0400

    Product size model

--- backend/app/models/sqlalchemy/product.py
+++ backend/app/models/sqlalchemy/product.py
@@ -7,11 +7,16 @@ Base = declarative_base()
 
 class ProductModel(Base):
     __tablename__ = 'products'
-
     product_id = Column("id", String, primary_key=True, index=True)
     product_type = Column("product_type", String, index=True)
     product_name = Column("product_name", String)
     price = Column("product_price", Float, nullable=False)
     description = Column("product_description", Text, nullable=True)
-    stock_quantity = Column("stock_quantity", Integer, nullable=False, default=0)
     image_url = Column("image_url", String, nullable=True)
+
+class ProductSizeModel(Base):
+    __tablename__ = 'product_sizes'
+    size_id = Column("id", Integer, primary_key=True)
+    product_id = Column("product_id", String, ForeignKey('products.id'), nullable=False)
+    size = Column("size", String)
+    stock_quantity = Column("stock_quantity", Integer, nullable=False, default=0)
