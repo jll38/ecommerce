1373595aa843b831f588efd511df8897f1db70e3
Julian Lechner <jll38@njit.edu>
2024-04-13 14:19:08 -0400

    Product display pagE

--- frontend/src/pages/product/ProductPage.tsx
+++ frontend/src/pages/product/ProductPage.tsx
@@ -6,6 +6,7 @@ import { getProductInfo } from "../../services/Product";
 export default function ProductPage() {
   const { productID } = useParams();
   const [productInfo, setProductInfo] = React.useState<any>();
+  const [loading, setLoading] = React.useState<boolean>(true);
   React.useEffect(() => {
     const fetchProduct = async () => {
       console.log(productID);
@@ -13,6 +14,7 @@ export default function ProductPage() {
         const product = await getProductInfo(productID);
         console.log(product);
         setProductInfo(product);
+        setLoading(false);
       }
     };
 
@@ -22,45 +24,52 @@ export default function ProductPage() {
   return (
     <main>
       <section id="product-info" className="flex gap-[3rem] justify-evenly">
-        <aside className="w-full max-w-[600px] h-[600px]">img here</aside>
-        <section
-          id="purchase-info"
-          className="w-full max-w-[400px] h-[600px] pt-2 px-0 flex flex-col gap-4"
-        >
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl font-semibold">Product Name</h2>
-            <h3 className="text-xl">$69.99</h3>
-            <p className="text-sm text-black/70">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-          <hr></hr>
-          <div className="flex flex-col gap-2">
-            <h4 className="text-sm text-black/70">Color:</h4>
-            <button>Test</button>
-          </div>
-          <hr></hr>
-          <div className="flex flex-col gap-2">
-            <h4 className="text-sm text-black/70">Size:</h4>
-            <button>Test</button>
-          </div>
-          <hr></hr>
-          <div className="flex flex-col gap-2">
-            <AddToCart />
-          </div>
-          <hr></hr>
-          <div className="flex flex-col gap-2">
-            <h4 className="text-md">Description</h4>
-            <p className="text-sm text-black/70">
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </section>
+        {loading ? (
+          <div>Loading</div>
+        ) : (
+          <>
+            <aside className="w-full max-w-[600px] h-[600px]">img here</aside>
+            <section
+              id="purchase-info"
+              className="w-full max-w-[400px] h-[600px] pt-2 px-0 flex flex-col gap-4"
+            >
+              <div className="flex flex-col gap-2">
+                <h2 className="text-2xl font-semibold">{productInfo.product_name}</h2>
+                <h3 className="text-xl">${productInfo.price}</h3>
+                <p className="text-sm text-black/70">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
+              <hr></hr>
+              <div className="flex flex-col gap-2">
+                <h4 className="text-sm text-black/70">Color:</h4>
+                <button>Test</button>
+              </div>
+              <hr></hr>
+              <div className="flex flex-col gap-2">
+                <h4 className="text-sm text-black/70">Size:</h4>
+                <button>Test</button>
+              </div>
+              <hr></hr>
+              <div className="flex flex-col gap-2">
+                <AddToCart route={productInfo["_links"].add_to_cart} />
+              </div>
+              <hr></hr>
+              <div className="flex flex-col gap-2">
+                <h4 className="text-md">Description</h4>
+                <p className="text-sm text-black/70">
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+            </section>
+          </>
+        )}
       </section>
     </main>
   );
