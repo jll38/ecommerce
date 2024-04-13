d39d8fcbbe6292a36636e784dc45e6ab94029738
Julian Lechner <jll38@njit.edu>
2024-04-13 14:27:16 -0400

    Product Service: getProductList Method

--- frontend/src/services/Product.ts
+++ frontend/src/services/Product.ts
@@ -1,8 +1,22 @@
 import { BACKEND_URL } from "../constants";
 
 export const getProductInfo = async (productID: string) => {
-  const product = await fetch(`${BACKEND_URL}/products/${productID}`).then(
-    (res) => res.json()
-  );
-  return product;
+  try {
+    const product = await fetch(`${BACKEND_URL}/products/${productID}`).then(
+      (res) => res.json()
+    );
+    return product;
+  } catch (err) {
+    console.error("Error fetching product information");
+  }
+};
+
+interface IGetProductList {
+  page: Number;
+  limit: Number;
+  type: string;
+}
+
+export const getProductList = async ({page, limit, type} : IGetProductList) => {
+    
 };
