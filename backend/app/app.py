89e00f4ff68288a292cf36b56807827afa5702a5
Julian Lechner <jll38@njit.edu>
2024-04-13 17:56:40 -0400

    API Docs

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.product_router import product_router
 from app.services.user_service import User_Service
-app = FastAPI()
+
+description = """
+Ecommerce  API 🔥
+
+You will be able to:
+* **Fetch Products from the Database**
+* **Add Items to cart** (_not implemented_)
+* **Manage Orders** (_not implemented_)
+
+
+"""
+app = FastAPI(title="E-Commerce API", description=description,
+              summary="The Backend to my E-Commerce Website",
+              contact={
+                  "name": "Julian Lechner",
+                  "url": "https://jlechner.com",
+                  "email": "julian@jlechner.com",
+              },)
+
 
 # User_Service.create_user(
 #     {"username": "user", "hashed_password": "password", "is_active": False})
