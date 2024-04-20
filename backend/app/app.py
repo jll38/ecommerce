655c95ca7c3cebd1f9e9efd3f311ef5f4d811df5
Julian Lechner <jll38@njit.edu>
2024-04-19 20:35:43 -0400

    Include routers

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers.product_router import product_router
+from app.routers.cart_router import cart_router
+from app.routers.support_router import support_router
+
 from app.services.user_service import User_Service
 from app.db import create_tables
 
@@ -35,6 +39,8 @@ app.add_middleware(
 )
 
 app.include_router(product_router)
+app.include_router(cart_router)
+app.include_router(support_router)
 
 create_tables()
 
